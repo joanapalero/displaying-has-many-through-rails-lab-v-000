d2a447417e1863839df6574736ec49fa5f55f94d
Joana Palero <joana.palero24@gmail.com>
2018-10-15 12:56:30 +0000

    automatically backed up by learn

--- db/migrate/20181015125609_create_appointments.rb
+++
@@ -1,9 +0,0 @@
-class CreateAppointments < ActiveRecord::Migration
-  def change
-    create_table :appointments do |t|
-      t.datetime :appointment_datetime
-      t.references :doctor, index: true, foreign_key: true
-      t.timestamps :false
-    end
-  end
-end
