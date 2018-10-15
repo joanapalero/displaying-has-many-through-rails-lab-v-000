6594a98774f22bce0224ad6cfc78f05c3493a87b
Joana Palero <joana.palero24@gmail.com>
2018-10-15 13:05:45 +0000

    automatically backed up by learn

---
+++ db/migrate/20181015130545_create_patients.rb
@@ -0,0 +1,11 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.string :department
+      t.timestamps :false
+
+      t.timestamps null: false
+    end
+  end
+end
